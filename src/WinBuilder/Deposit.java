f50c46982debbbc80f708d7d6e8139526c8c8f47
DallingtonAsin <asingwire50dallington@gmail.com>
2021-11-23 16:56:47 +0300

    added db connection

---
+++ src/WinBuilder/Deposit.java
@@ -0,0 +1,20 @@
+package WinBuilder;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class Deposit extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Deposit() {
+		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("DEPOSIT MONEY ON YOUR ACCOUNT");
+		lblNewLabel.setBounds(102, 18, 299, 24);
+		add(lblNewLabel);
+
+	}
+
+}
